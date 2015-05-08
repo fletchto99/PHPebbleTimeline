0a4b0d276e21b881f68442881c381169253d3a70
Matthew Langlois <fletchto99@gmail.com>
2015-05-07 21:16:25 -0400

    Add a readme and sample Further documentation coming later

---
+++ Readme.md
@@ -0,0 +1,4 @@
+
+## How To Use
+
+Place the TimelineAPI folder in your projects root directory. Be sure to include Timeline.php, the rest will be loaded automatically.
---
+++ Sample.php
@@ -0,0 +1,30 @@
+<?php
+
+//Include the timeline API
+require_once 'TimelineAPI/Timeline.php';
+
+//Import the required classes
+use TimelineAPI\Pin;
+use TimelineAPI\PinLayout;
+use TimelineAPI\PinLayoutType;
+use TimelineAPI\PinIcon;
+use TimelineAPI\PinReminder;
+use TimelineAPI\Timeline;
+
+//Create some layouts which our pin will use
+$reminderlayout = new PinLayout(PinLayoutType::GENERIC_PIN, 'Sample timline pin!', null, null, null, PinIcon::NOTIFICATION_FLAG);
+$pinlayout = new PinLayout(PinLayoutType::GENERIC_PIN, 'Our title', null, null, null, PinIcon::NOTIFICATION_FLAG);
+
+//Create a reminder which our pin will push before the event
+$reminder = new PinReminder($reminderlayout, (new DateTime('now')) -> add(new DateInterval('PT10M')));
+
+//Create the pin
+$pin = new Pin('sample-pin-1', (new DateTime('now')) -> add(new DateInterval('PT5M')), $layout);
+
+//Attach the reminder
+$pin -> addReminder($reminder);
+
+//Push the pin to the timeline
+Timeline::pushPin('sample-userToken', $pin);
+
+?>
